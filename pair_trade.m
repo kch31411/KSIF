042458bc0b3af202a6be619dc980c3acde795c2d
Changha <kch31411@business.kaist.ac.kr>
2014-07-13 22:33:23 +0900

    Initial commit w/ simple data read

---
+++ pair_trade.m
@@ -0,0 +1,27 @@
+tic;
+
+clc; clear; close all;
+
+% 데이터 읽어오기 
+% 지금은 fnDataGuide에서 주는 포맷을 따름.
+[ndata, text, alldata] = xlsread('개별_주식선물.xlsx');
+
+date = datenum(text(15:end,1), 'yyyy-mm-dd');
+
+name = text(10, 2:end);
+% 종목이름 F 연결선물지수 -> 종목이름
+name = arrayfun(@(x) strread(x{1}, '%s', 'delimiter', ' '), name, 'UniformOutput', false);   % @(x) strsplit(x{1}) in current version of matlab 
+name = arrayfun(@(x) x{1}{1}, name, 'UniformOutput', false);
+
+price = ndata(8:end,:);
+price = log(price);    % conver to log scale
+
+% data validation check
+num_date = size(date, 1);
+assert(num_date == size(price, 1), 'ERROR : date size does not match');
+num_asset = size(name, 2);
+assert(num_asset == size(price, 2), 'ERROR : asset size does not match');
+
+
+
+toc
